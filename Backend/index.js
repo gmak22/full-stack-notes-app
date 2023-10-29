4c9a94a6c984dc66a2d74d8c07b86032aeb47617
unknown <makarand.gijam22@gmail.com>
2023-10-29 23:03:19 +0530

    changed connection logic

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,37 @@
-const express =  require("express")
-const { connection } = require("./db")
-const { userRouter } = require("./routes/user.routes")
-const { noteRouter } = require("./routes/note.routes")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.routes");
+const { noteRouter } = require("./routes/note.routes");
+const cors = require("cors");
+require("dotenv").config();
 
-const app =  express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use("/users",userRouter)
-app.use("/notes",noteRouter)
+app.use(express.json());
+app.use("/users", userRouter);
+app.use("/notes", noteRouter);
 
-app.get("/",(req,res)=>{
-    res.send("Testing Server OK")
-})
+app.get("/", (req, res) => {
+  res.send("Testing Server OK");
+});
 
-app.listen(process.env.PORT, async()=>{
-    try{
-        await connection
-        console.log(`Server is running at PORT ${process.env.PORT}` )
-    }
-    catch(err){
-        console.log("Error in connecting to DB")
-        console.log(err)
-    }
-})
+try {
+  (async function () {
+    await connection;
+  })();
+  console.log(`Server is connected to DB`);
+} catch (err) {
+  console.log("Error in connecting to DB");
+  console.log(err);
+}
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log(`Server is running at PORT ${process.env.PORT}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
